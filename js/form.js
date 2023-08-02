215e729ac58b7a764359b9a24171ab06e5bb5d53
Julio Rezende <juliorezende@bemol.com.br>
2023-08-01 23:13:11 -0400

    Fix: Forms send emails and include new images

--- js/form.js
+++ js/form.js
@@ -1,23 +1,41 @@
 $("#submit").click(function(){
-    var name = $("#name").val();
-    var email = $("#email").val();
-    var message = $("#message").val();
-    
-    if(name == '' || email == '' || message == ''){
-        swal({
+  var name = $("#name").val();
+  var email = $("#email").val();
+  var message = $("#message").val();
+
+  // Verifica se o e-mail contém o caractere "@"
+  var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailPattern.test(email)) {
+      swal({
           title: "Opas!",
-          text: "Precisamos que todos os campos sejam preenchidos para enviarmos sua mensagem!",
+          text: "Por favor, insira um endereço de e-mail válido!",
           icon: "warning",
           button: "fechar",
-        });
-        return false
-    } else{
+      });
+      return false;
+  }
   
-        swal({
-          title: "Mensagem enviada com sucesso!",
-          text: "Agradecemos sua mensagem, vamos contatar você assim que possível pelo seu e-mail informado!",
-          icon: "success",
-          button: "fechar",
-        });
-    }
-})
+  if(name === '' || email === '' || message === ''){
+      swal({
+        title: "Opas!",
+        text: "Precisamos que todos os campos sejam preenchidos para enviarmos sua mensagem!",
+        icon: "warning",
+        button: "fechar",
+      });
+      return false;
+  } else{
+      swal({
+        title: "Mensagem enviada com sucesso!",
+        text: "Agradecemos sua mensagem, vamos contatar você assim que possível pelo seu e-mail informado!",
+        icon: "success",
+        button: "fechar",
+      });
+
+      setTimeout(function() {
+        $("#name").val("");
+        $("#email").val("");
+        $("#message").val("");
+    }, 2000);
+
+  }
+});
